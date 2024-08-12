cad064c9a43e673ba03adff7eb665aad471057ca
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2024-08-12 15:32:07 -0500

    update extrensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // Theme from daisyui https://daisyui.com/
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
