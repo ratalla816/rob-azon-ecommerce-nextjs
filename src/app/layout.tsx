6bc80309235e449dc519f3a84c5d87d30179b8fd
ratalla816 <rob.atalla@robatalla816.com>
2024-09-05 20:02:18 -0500

    updated components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Footer from "./Footer";
+import Navbar from "./Navbar/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import SessionProvider from "./SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Rob-Azon!",
-  description: "Dive Deep and Deliver Results - We start With All That Sweet Money and Work Backwards",
+export const metadata = {
+  title: "Flowmazon",
+  description: "We make your wallet cry",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* RESPONSIVE CSS */}
-        <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+        <SessionProvider>
+          <Navbar />
+          <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
