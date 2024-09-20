08db4d6c09ccec030c17823fb541aa16eea3dc32
ratalla816 <rob.atalla@robatalla816.com>
2024-09-19 21:28:27 -0500

    updated config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
     remotePatterns: [
       { hostname: "images.unsplash.com" },
       { hostname: "lh3.googleusercontent.com" },
+      { hostname: "https://ratalla816.github.io/robazon-pics/assets/images/squirrel.jpg" },
+      { hostname: "https://ratalla816.github.io/robazon-pics/assets/images/spaghetti-hat.jpg" },
+      { hostname: "https://ratalla816.github.io/robazon-pics/assets/images/ship-bottle.jpg" },
+      { hostname: "https://ratalla816.github.io/robazon-pics/assets/images/plant.jpg" },
+      { hostname: "https://ratalla816.github.io/robazon-pics/assets/images/luxury-tank.jpg" },
+      { hostname: "https://ratalla816.github.io/robazon-pics/assets/images/fish-shoes.jpg" },
+      { hostname: "https://ratalla816.github.io/robazon-pics/assets/images/cobra.jpg" },
     ],
   },
   experimental: {
